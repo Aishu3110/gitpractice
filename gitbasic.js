dbbf2b09a361c6244bcdf01f45f1f0cf75662494
Aishu3110 <aishwarya@pyramidions.in>
2023-10-18 15:34:40 +0530

    save the file

---
+++ gitbasic.js
@@ -0,0 +1,15 @@
+// git init
+// git is initialized in the folder
+
+// git status
+// no commits yet 
+
+// git add .
+// all files will be added
+
+// git add filename
+// only the specified file will be added to the git repo
+
+// git status
+// if files added means it will show the file has green and it will be as changes commited
+
